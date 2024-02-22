77d27fe2235eb3fd85360283557a6b5fca2d1190
dilya-gitit <dilnas.yerkemblan@nu.edu.kz>
2024-02-22 22:09:57 +0600

    Done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder sb = new StringBuilder();
+        try (java.io.FileReader fr = new java.io.FileReader(file)){
+            int data;
+            while ((data =fr.read()) != -1) {
+                sb.append((char)data);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] tokens = sb.toString().split("\\s*[:\\n]\\s*");
+        for (String s : tokens) {
+            System.out.println(s);
+        }
+        Profile pr = getProfile(tokens);
+        return pr;
+    }
+
+    private static Profile getProfile(String[] tokens) {
+        Profile pr = new Profile();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("Name")) {
+                pr.setName(tokens[i+1]);
+            }
+            if (tokens[i].equals("Age")) {
+                pr.setAge(Integer.parseInt(tokens[i+1]));
+            }
+            if (tokens[i].equals("Email")) {
+                pr.setEmail(tokens[i+1]);
+            }
+            if (tokens[i].equals("Phone")) {
+                pr.setPhone((long)Integer.parseInt(tokens[i+1]));
+            }
+        }
+        return pr;
     }
 }
